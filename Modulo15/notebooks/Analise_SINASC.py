ce50c5497a596bf67b8bcef35dc0eb062e440b35
Marcelo Otsuki <marceloaires7@gmail.com>
2023-07-15 20:01:15 -0300

    Modulo15

--- Modulo15/notebooks/Analise_SINASC.py
+++ Modulo15/notebooks/Analise_SINASC.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import streamlit as st
 import os
-import sys
 
 def plota_pivot_table(df, value, index, func, ylabel, xlabel, opcao='nada'):
     if opcao == 'nada':
@@ -14,39 +14,26 @@ def plota_pivot_table(df, value, index, func, ylabel, xlabel, opcao='nada'):
         pd.pivot_table(df, values=value, index=index, aggfunc=func).unstack().plot(figsize=[15, 5])
     plt.ylabel(ylabel)
     plt.xlabel(xlabel)
+    st.pyplot(fig=plt)
     return None
 
-print('Esse Script se chama: '+sys.argv[0])
+st.write('# Análise SINASC')
 
-meses = sys.argv[1:]
+sinasc = pd.read_csv('./input/SINASC_RO_2019.csv')
 
-print(f'Vamos trabalhar com os meses: {sys.argv[1:]}')
+sinasc.DTNASC = pd.to_datetime(sinasc.DTNASC)
 
-for mes in meses:
+min_data = sinasc.DTNASC.min()
+max_data = sinasc.DTNASC.max()
 
-    sinasc = pd.read_csv('./input/SINASC_RO_2019_'+ mes +'.csv')
-    print(sinasc.DTNASC.min(), sinasc.DTNASC.max())
+st.write(min_data)
+st.write(max_data)
 
-    max_data = sinasc.DTNASC.max()[:7]
-    print(max_data)
-    os.makedirs('./output/figs/'+max_data, exist_ok=True)
 
-    plota_pivot_table(sinasc, 'IDADEMAE', 'DTNASC', 'count', 'quantidade de nascimento','data de nascimento')
-    plt.savefig('./output/figs/'+max_data+'/quantidade de nascimento.png')
 
-    plota_pivot_table(sinasc, 'IDADEMAE', ['DTNASC', 'SEXO'], 'mean', 'media idade mae','data de nascimento','unstack')
-    plt.savefig('./output/figs/'+max_data+'/media idade mae por sexo.png')
-
-    plota_pivot_table(sinasc, 'PESO', ['DTNASC', 'SEXO'], 'mean', 'media peso bebe','data de nascimento','unstack')
-    plt.savefig('./output/figs/'+max_data+'/media peso bebe por sexo.png')
-
-    plota_pivot_table(sinasc, 'PESO', 'ESCMAE', 'median', 'apgar1 medio','gestacao','sort')
-    plt.savefig('./output/figs/'+max_data+'/media apgar1 por escolaridade mae.png')
-
-    plota_pivot_table(sinasc, 'APGAR1', 'GESTACAO', 'mean', 'apgar1 medio','gestacao','sort')
-    plt.savefig('./output/figs/'+max_data+'/media apgar1 por gestacao.png')
-
-    plota_pivot_table(sinasc, 'APGAR5', 'GESTACAO', 'mean', 'apgar5 medio','gestacao','sort')
-    plt.savefig('./output/figs/'+max_data+'/media apgar5 por gestacao.png')
-    
-    plt.close()
+plota_pivot_table(sinasc, 'IDADEMAE', 'DTNASC', 'mean', 'quantidade de nascimento','data de nascimento')
+plota_pivot_table(sinasc, 'IDADEMAE', ['DTNASC', 'SEXO'], 'mean', 'media idade mae','data de nascimento','unstack')
+plota_pivot_table(sinasc, 'PESO', ['DTNASC', 'SEXO'], 'mean', 'media peso bebe','data de nascimento','unstack')
+plota_pivot_table(sinasc, 'PESO', 'ESCMAE', 'median', 'apgar1 medio','gestacao','sort')
+plota_pivot_table(sinasc, 'APGAR1', 'GESTACAO', 'mean', 'apgar1 medio','gestacao','sort')
+plota_pivot_table(sinasc, 'APGAR5', 'GESTACAO', 'mean', 'apgar5 medio','gestacao','sort')
