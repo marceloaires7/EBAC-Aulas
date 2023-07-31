c0b1a53dc41078d6c79f292e23f392c789a1105d
Marcelo Otsuki <marceloaires7@gmail.com>
2023-07-31 17:42:52 -0300

    PROJETO2

---
+++ Modulo16/notebooks/projeto 2/renda_grafico.py
@@ -0,0 +1,29 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+def renda_countplot(x, data, ax, title=''):
+    sns.countplot(x=x,
+                  data=data,
+                  ax=ax,)
+    ax.set_title(title)
+    ax.tick_params(axis='x',
+                   rotation=45)
+
+def renda_lineplot(x, y, hue, data, ax, title=''):
+    sns.lineplot(x=x,
+                 y=y,
+                 hue=hue,
+                 data=data,
+                 ax=ax)
+    ax.set_title(title)
+    ax.tick_params(axis='x',
+                   rotation=45)
+    
+def renda_barplot(x, y, data, ax, title=''):
+    sns.barplot(x=x,
+                y=y,
+                data=data,
+                ax=ax)
+    ax.set_title(title)
+    ax.tick_params(axis='x',
+                   rotation=45)
